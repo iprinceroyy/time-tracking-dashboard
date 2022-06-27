9fdc53af9ad11da49613b10b7bf53bec579a4e76
Prince Roy <princeku.ray143@gmail.com>
2022-06-27 16:35:05 +0530

    update script.js

--- js/script.js
+++ js/script.js
@@ -7,17 +7,23 @@ const formatClassName = word => word.toLowerCase().split(' ').join('-');
 const btnDaily = document.querySelector('.js--daily');
 const btnWeekly = document.querySelector('.js--weekly');
 const btnMonthly = document.querySelector('.js--monthly');
+const activity = document.querySelector('.activity');
 
-fetch('./data.json')
-    .then(Response => Response.json())
-    .then(jsonData => {
-        jsonData.forEach(data => {
-            let html;
-            const addhtml = timeSpan => {
-                html = `<section class="activity activity--${formatClassName(
-          data.title
-        )}">
-            <div class="activity__status">
+let prev = [];
+let curr = [];
+let next = [];
+const states = ['daily', 'weekly', 'monthly'];
+const displayData = tar => {
+    fetch('./data.json')
+        .then(Response => Response.json())
+        .then(jsonData => {
+            jsonData.forEach(data => {
+                let html;
+                const addhtml = timeSpan => {
+                    html = `<section class="activity activity--${formatClassName(
+            data.title
+          )}">
+            <div class="activity__status ${tar}">
                 <h2 class="activity__title">${data.title}</h2>
                 <hr>
                 <p class="current status__current__${formatClassName(
@@ -29,8 +35,48 @@ fetch('./data.json')
             </div>
         </section>`;
 
-                return html;
-            };
-            dashboard.insertAdjacentHTML('beforeend', addhtml('daily'));
+                    return html;
+                };
+
+                // Call addHtml with target day
+                dashboard.insertAdjacentHTML('beforeend', addhtml(tar));
+            });
+
+            const tests = document.querySelectorAll(`.${tar}`);
+
+            if (curr) {
+                const currEle = document.querySelectorAll(`.${curr[0]}`);
+                currEle.forEach(ce => {
+                    ce.classList.remove('active');
+                });
+            }
+
+            if (prev) {
+                const prevEle = document.querySelectorAll(`.${prev[0]}`);
+                console.log(prevEle);
+                prevEle.forEach(pe => {
+                    pe.classList.remove('active');
+                });
+            }
+
+            tests.forEach(test => {
+                test.classList.add('active');
+                curr.push(test.classList[1]);
+            });
+            prev = curr;
         });
+};
+
+const btns = document.querySelectorAll('.btn');
+btns.forEach(btn => {
+    // For Each btn add Event handler that calls displayData
+    btn.addEventListener('click', e => {
+        const activeElement = e.currentTarget.innerText.toLowerCase();
+        displayData(activeElement);
     });
+});
+
+// Default display
+window.addEventListener('load', () => {
+    displayData('daily');
+});

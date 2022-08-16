ef2f09f1312c7801b5815e4633c78a331ae2f17b
Prince Roy <princeku.ray143@gmail.com>
2022-08-16 23:29:58 +0530

    create animation event handler

--- js/script.js
+++ js/script.js
@@ -96,3 +96,15 @@ day.addEventListener('click', e => {
 
 // Initially display data for weekly
 fetchData('weekly');
+
+// After display
+document.querySelectorAll('.activity').forEach(act => {
+    act.addEventListener('click', e => {
+        console.log(e);
+    });
+});
+
+// Animate card
+dashboard.addEventListener('click', e => {
+    e.target.closest('.activity').classList.toggle('animate');
+});
